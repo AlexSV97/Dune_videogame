a6b587269a9b8cac51361d5166b43e39b425b699
Usuario 16 Aula 8 tarde <A8-16t@dtoledo.local>
2026-04-13 19:18:16 +0200

    Juego Demo v.1

--- dune-arrakis-dominion/scripts/camera_controller.gd
+++
@@ -1,9 +0,0 @@
-extends Camera2D
-
-@export var target: Node2D
-@export var follow_speed: float = 5.0
-@export var camera_offset: Vector2 = Vector2(0, -50)
-
-func _physics_process(delta):
-	if target:
-		global_position = global_position.lerp(target.global_position + camera_offset, follow_speed * delta)
